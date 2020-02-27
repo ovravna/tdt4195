10b396c172d593bc3de430faa64c1ac97772a0f5
Ole Ravna <olevra@stud.ntnu.no>
2020-02-27 15:24:27 +0100

    Add task 1c

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -4,32 +4,51 @@
 #include <vector>
 #include "gloom/shader.hpp"
 
-/* std::vector<float> vertices= { */ 
-/* 	-0.6, -0.6, 0, */
-/* 	0.6, -0.6, 0, */
-/* 	0, 0.6, 0 */
+std::vector<float> vertices = { 
+	-0.6, -0.6, 0,
+	0.6, -0.6, 0,
+	0, 0.6, 0
 
 
-/* 	/1* -0.5f, -0.5f, 0, *1/ */
-/* 	/1*  0.0f,  0.5f, 0, *1/ */
-/* 	/1*  0.5f, -0.5f, 0 *1/ */
+	/* -0.5f, -0.5f, 0, */
+	/*  0.0f,  0.5f, 0, */
+	/*  0.5f, -0.5f, 0 */
+};
+std::vector<int> indices = { 1, 2, 0 };
+
+std::vector<float> verts = {
+
+	0.0, 0.0, 0.0,  //0
+	-.6, 0, 0.,		//1
+	.6, 0, 0.,		//2
+	0., -.6, 0.,	//3
+	0., .6, 0.,		//4
+	.6, .6, 0.,		//5
+};
+
+
+std::vector<int> inds = {
+	0, 4, 1,
+	0, 2, 4,
+	0, 3, 2,  
+	0, 1, 3,
+	4, 2, 5
+
+};
+
+/* std::vector<float> vertices{ */
+/* 									-0.9,-0.9,0, */
+/* 									0,0.9,0, */
+/* 									0.5,-0.9,0, */
+/* 									0.6,-0.9,0, */
+/* 									0.9,0.9,0, */
+/* 									0.1,0.9,0 */
 /* }; */
-/* std::vector<int> indices = { 2, 1, 0 }; */
 
-
-std::vector<float> vertices{
-									-0.9,-0.9,0,
-									0,0.9,0,
-									0.5,-0.9,0,
-									0.6,-0.9,0,
-									0.9,0.9,0,
-									0.1,0.9,0
-};
-
-std::vector<int> indices{	
-	2,1,0,
-	3,4,5
-};
+/* std::vector<int> indices{ */	
+/* 	2,1,0, */
+/* 	3,4,5 */
+/* }; */
 
 
 unsigned int setupVAO(std::vector<float> vertexCoordinates, std::vector<int> indices) {
@@ -46,8 +65,8 @@ unsigned int setupVAO(std::vector<float> vertexCoordinates, std::vector<int> ind
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexCoordinates.size(), vertexCoordinates.data(), GL_STATIC_DRAW); 
 
 
-	unsigned int index = 1;
-	glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, 12, 0);
+	unsigned int index = 0;  // todo: why does this need to be 0?????
+	glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(index);
 
 	unsigned int indexBuffer = 0;
@@ -62,10 +81,8 @@ unsigned int setupVAO(std::vector<float> vertexCoordinates, std::vector<int> ind
 
 Gloom::Shader * loadShader() {
 
-	Gloom::Shader * shader; // = new Gloom::Shader();
-	shader->attach("gloom/shaders/simple.frag");
-	shader->attach("gloom/shaders/simple.vert");
-	shader->link();
+	Gloom::Shader * shader = new Gloom::Shader(); 
+	shader->makeBasicShader("gloom/shaders/simple.frag", "gloom/shaders/simple.vert");
 	return shader;
 
 }
@@ -82,34 +99,12 @@ void runProgram(GLFWwindow* window)
     // Set default colour after clearing the colour buffer
     glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
 
-	Gloom::Shader * shader = new Gloom::Shader(); // = new Gloom::Shader();
-	shader->makeBasicShader("gloom/shaders/simple.frag", "gloom/shaders/simple.vert");
+	auto shader = loadShader();
 
     // Set up your scene here (create Vertex Array Objects, etc.)
 	/* unsigned int vertexArray = setupVAO(triangle_vertices, triangle_indices); */
 	
-	unsigned int vertexArray = 0;
-	glGenVertexArrays(1, &vertexArray);
-	glBindVertexArray(vertexArray);
-	
-	unsigned int vertexBuffer = 0; 
-	glGenBuffers(1, &vertexBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexArray);
-
-	/* float vertices[] = {1.0, 3.0, 2.0, 5.0, 4.0, 3.0, 2.0, 6.0, 3.0}; */
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW); 
-
-
-	unsigned int index = 0;
-	glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, 12, 0);
-	glEnableVertexAttribArray(index);
-
-	unsigned int indexBuffer = 0;
-	glGenBuffers(1, &indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * indices.size(), indices.data(), GL_STATIC_DRAW);
-	
-	printGLError();
+	unsigned int vertexArray = setupVAO(verts, inds);
 	
 	shader->activate();
 
@@ -123,7 +118,7 @@ void runProgram(GLFWwindow* window)
         // Draw your scene here
 		/* glDrawArrays(GL_TRIANGLES, 0, 3); */
 		glBindVertexArray(vertexArray);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, inds.size(), GL_UNSIGNED_INT, 0);
 
         // Handle other events
         glfwPollEvents();
